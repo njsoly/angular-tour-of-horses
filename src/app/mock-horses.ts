1c73287ce89cc9dc474149d141bb0867a9debc31
Nate Solyntjes <nate.solyntjes@bestbuy.com>
2018-12-10 13:14:54 -0600

    +mock-horses.ts: new file to fake a mock list of horses.

---
+++ src/app/mock-horses.ts
@@ -0,0 +1,15 @@
+import { Horse } from './horses';
+
+export const HORSES: Horse[] = [
+    {id: 11, name: 'BoJack'},
+    {id: 12, name: 'Paul Revere'},
+    {id: 13, name: 'Butterscotch'},
+    {id: 14, name: 'Secratariat'},
+    {id: 15, name: 'Jim the White Line'},
+    {id: 16, name: 'Snowballs'},
+    {id: 17, name: 'Didgeridoo'},
+    {id: 18, name: 'Dewford'},
+    {id: 19, name: 'Blenderson'},
+    {id: 20, name: 'Snuggly Douglas'},
+    
+];
