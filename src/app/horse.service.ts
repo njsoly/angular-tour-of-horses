1e929329077e6a61ff24751f8c2ade95b44333d0
Nate Solyntjes <nate.solyntjes@bestbuy.com>
2018-12-11 11:52:21 -0600

    aoeu

--- src/app/horse.service.ts
+++ src/app/horse.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
+
+/**
+ * This thing is going to provide an injectable service.
+ */
 export class HorseService {
 
   constructor() { }
