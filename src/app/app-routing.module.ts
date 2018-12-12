d39d20d67c0cc4c15104e7dd754afb16fd0c02e5
Nate Solyntjes <nate.solyntjes@bestbuy.com>
2018-12-12 13:40:53 -0600

    AppRouting: comments.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HorsesComponent } from './horses/horses.component';
 
+/**
+ * This is the "AppRoutingModule" that you've heard so much about.
+ */
+
 const routes: Routes = [
   /** this will match the URL 'horses' and link you 
    * over to "HorsesComponent".
@@ -11,6 +15,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  exports: [ RouterModule ],
 })
 export class AppRoutingModule { }
