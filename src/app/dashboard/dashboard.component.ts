766028c5e5a5cabda621095eabf38249b8999177
Nate Solyntjes <nate.solyntjes@bestbuy.com>
2018-12-12 13:32:16 -0600

    dashboard: pasted in the given HTML template and TypeScript code.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Horse } from '../horse';
+import { HorseService } from '../horse.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: [ './dashboard.component.css' ]
 })
 export class DashboardComponent implements OnInit {
+  horses: Horse[] = [];
 
-  constructor() { }
+  constructor(private horseService: HorseService) { }
 
   ngOnInit() {
+    this.getHorses();
   }
 
+  getHorses(): void {
+    this.horseService.getHorses()
+      .subscribe(horses => this.horses = horses.slice(1, 5));
+  }
 }
