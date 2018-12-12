a6ba6e430eec06e0bacaa93da8c4b802b395bdac
Nate Solyntjes <nate.solyntjes@bestbuy.com>
2018-12-12 13:03:35 -0600

    app-routing-module: new import, and my very first Route!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HorsesComponent } from './horses/horses.component';
+
+const routes: Routes = [
+  // this will match the URL 'horses' and link you 
+  // over to "HorsesComponent".
+  { path: 'horses', component: HorsesComponent } 
+];
 
 @NgModule({
   exports: [ RouterModule ]
