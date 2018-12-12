116c803a55f3bf7355efd6c0cded43f3b3e8eb18
Nate Solyntjes <nate.solyntjes@bestbuy.com>
2018-12-12 13:44:02 -0600

    added dashboard route to the AppRoutingModule.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HorsesComponent } from './horses/horses.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 /**
  * This is the "AppRoutingModule" that you've heard so much about.
  */
 
+/* this array is referred to as: 
+ * "the AppRoutingModule.routes array".
+ */
 const routes: Routes = [
   /** this will match the URL 'horses' and link you 
    * over to "HorsesComponent".
    */
-  { path: 'horses', component: HorsesComponent } 
+  { path: 'horses', component: HorsesComponent },
+
+  /*  */
+  { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
